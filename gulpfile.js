fe9295d6167587288d410b152531d9ae5a69d945
LeonWebSolution <leonwebdevv@gmail.com>
2023-11-13 08:33:10 +0300

    f

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const { src, dest, watch, parallel, series} = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+// const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+
+function images(){
+    return src(['src/images/src/*.*'])
+    .pipe(src('src/images/src/*.*'))
+    .pipe(newer('src/images'))
+    .pipe(imagemin())
+    .pipe(dest('src/images'))
+}
+
+function styles() {
+    return src('src/scss/**/*.scss')
+    // .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version']}))
+    .pipe(concat('style.min.css'))
+    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(dest('src/css'))
+    .pipe(browserSync.stream())
+}
+
+function scripts() {
+    return src([
+        'src/js/components/*.js',
+        '!src/js/app.js',
+    ])
+    .pipe(concat('app.js'))
+    .pipe(dest('src/js'))
+    .pipe(browserSync.stream())
+}
+
+function building(){
+    return src([
+        'src/css/style.min.css',
+        'src/images/*.*',
+        'src/js/*',
+        'src/*.html'
+    ], {base: 'src'})
+    .pipe(dest('dist'))
+}
+
+function cleanDist() {
+    return src('dist')
+        .pipe(clean())
+}
+
+function watching() {
+    browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
+    watch(['src/scss/**/*.scss'], styles)
+    watch(['src/images/'], images)
+    watch(['src/js/components/*.js'], scripts)
+    watch(['src/*.html']).on('change', browserSync.reload)
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching =  watching;
+exports.building =  building;
+exports.images =  images;
+
+exports.build = series(cleanDist, building);
+exports.default = parallel(styles, scripts, watching);
