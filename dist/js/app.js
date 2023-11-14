06c61c822d6fa606dc61f23b91ffcf824f18a26f
LeonWebSolution <leonwebdevv@gmail.com>
2023-11-14 17:59:54 +0300

    sliders

--- dist/js/app.js
+++ dist/js/app.js
@@ -62,7 +62,52 @@ var solutions = new Swiper('.solutions__slider', {
   initialSlide: 0, 
   centerInsufficientSlides: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '#swiper-button-next-sol',
+    prevEl: '#swiper-button-prev-sol',
   },
+});
+
+var work = new Swiper('.work__slider', {
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  initialSlide: 0, 
+  centerInsufficientSlides: true,
+  navigation: {
+    nextEl: '#swiper-button-next-work',
+    prevEl: '#swiper-button-prev-work',
+  },
+});
+
+var projects = new Swiper('.projects__slider', {
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  initialSlide: 0, 
+  centerInsufficientSlides: true,
+  navigation: {
+    nextEl: '#swiper-button-next-proj',
+    prevEl: '#swiper-button-prev-proj',
+  },
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Получаем все вкладки и слайдеры
+  var tabs = document.querySelectorAll('.projects__tab');
+  var sliders = document.querySelectorAll('.projects__slider');
+
+  // Добавляем обработчик событий click к каждой вкладке
+  tabs.forEach(function(tab, index) {
+      tab.addEventListener('click', function() {
+          // Удаляем класс 'active' у всех вкладок и слайдеров
+          tabs.forEach(function(t) {
+              t.classList.remove('active');
+          });
+          sliders.forEach(function(s) {
+              s.classList.remove('active');
+          });
+
+          // Добавляем класс 'active' к кликнутой вкладке и соответствующему слайдеру
+          this.classList.add('active');
+          sliders[index].classList.add('active');
+      });
+  });
 });
