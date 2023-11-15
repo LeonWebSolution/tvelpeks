213fe1f4a8475312f81852278182a041177721c8
LeonWebSolution <leonwebdevv@gmail.com>
2023-11-15 11:09:19 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var dropdowns = document.querySelectorAll('.footer__nav--list');
+
+  dropdowns.forEach(function (dropdown) {
+      var dropdownName = dropdown.querySelector('.footer__nav--list-name');
+      var dropdownContent = dropdown.querySelector('.footer__nav--list-b');
+
+      dropdownName.addEventListener('click', function () {
+          // Проверяем, есть ли у элемента класс 'all-list'
+          if (!dropdown.classList.contains('all-list')) {
+              dropdown.classList.toggle('active');
+
+              if (dropdown.classList.contains('active')) {
+                  // Вычисляем высоту контента и присваиваем max-height
+                  dropdownContent.style.maxHeight = dropdownContent.scrollHeight + "px";
+              } else {
+                  // Убираем max-height при закрытии
+                  dropdownContent.style.maxHeight = null;
+              }
+          }
+      });
+  });
+});
+
 window.addEventListener("scroll", function() {
     var header = document.querySelector(".header");
     var sectionMain = document.querySelector(".main__section");
@@ -107,6 +131,31 @@ var partners = new Swiper('.partners__slider', {
   centerInsufficientSlides: true,
   loop: true,
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.innerWidth < 962) {
+      var advantage = new Swiper('.advantages__slider', {
+          slidesPerView: 'auto',
+          spaceBetween: 30,
+          initialSlide: 0, 
+          centerInsufficientSlides: true,
+          loop: true,
+      });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 document.addEventListener("DOMContentLoaded", function() {
