411d3b09bbeb678781048da09179624e713a12b0
LeonWebSolution <leonwebdevv@gmail.com>
2023-11-16 13:55:08 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -33,6 +33,25 @@ window.addEventListener("scroll", function() {
     sectionMain.classList.remove("fixed");
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var myElement = document.querySelector('.header');
+  var lastScrollTop = 0;
+
+  window.addEventListener('scroll', function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop && scrollTop > 500) {
+      // Скролл вниз
+      myElement.classList.add('hidden');
+    } else {
+      // Скролл вверх или на самом верху
+      myElement.classList.remove('hidden');
+    }
+
+    lastScrollTop = scrollTop;
+  });
+});
     /**
  * Swiper 11.0.4
  * Most modern mobile touch slider and framework with hardware accelerated transitions
