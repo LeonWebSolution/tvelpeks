72d5beac5403a8050683c40d94b396897a06ef80
LeonWebSolution <leonwebdevv@gmail.com>
2023-11-19 11:16:29 +0300

    prav

--- src/js/app.js
+++ src/js/app.js
@@ -260,25 +260,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
   });
-document.getElementById('supportForm').addEventListener('submit', function(event) {
-    var phoneNumberInput = document.getElementById('phoneNumber');
-    var inputErrorLabel = document.querySelector('.input-arror');
+// document.getElementById('supportForm').addEventListener('submit', function(event) {
+//     var phoneNumberInput = document.getElementById('phoneNumber');
+//     var inputErrorLabel = document.querySelector('.input-arror');
 
-    // Regular expression to check if the phone number is in the correct format
-    var phoneNumberPattern = /^\d{10}$/;
+//     // Regular expression to check if the phone number is in the correct format
+//     var phoneNumberPattern = /^\d{10}$/;
 
-    if (!phoneNumberPattern.test(phoneNumberInput.value)) {
-        // If the phone number is not in the correct format, add the 'visible' class to the error label
-        inputErrorLabel.classList.add('visible');
-        phoneNumberInput.value = ''; // Clear the input value
-        event.preventDefault(); // Prevent form submission
-    } else {
-        // If the phone number is in the correct format, remove the 'visible' class from the error label
-        inputErrorLabel.classList.remove('visible');
-    }
-});
-
-
+//     if (!phoneNumberPattern.test(phoneNumberInput.value)) {
+//         // If the phone number is not in the correct format, add the 'visible' class to the error label
+//         inputErrorLabel.classList.add('visible');
+//         phoneNumberInput.value = ''; // Clear the input value
+//         event.preventDefault(); // Prevent form submission
+//     } else {
+//         // If the phone number is in the correct format, remove the 'visible' class from the error label
+//         inputErrorLabel.classList.remove('visible');
+//     }
+// });
 
 
 
