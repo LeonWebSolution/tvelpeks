3baf87b65e302f09c52d44ff6fe3ac7dc27566ff
LeonWebSolution <leonwebdevv@gmail.com>
2023-11-20 09:11:51 +0300

    index

--- src/js/app.js
+++ src/js/app.js
@@ -88,73 +88,54 @@ function closeModal() {
   modal.classList.remove('active'); 
   body.style.overflow = ''; 
 }
-document.addEventListener("DOMContentLoaded", function() {
-    function addActiveClasses(element, list, blurLink) {
-        element.classList.add('active');
-        list.classList.add('active');
-        blurLink.classList.add('active');
-    }
+const minScreenWidth = 962;
+const solutionsLinks = document.querySelectorAll('.solutions__name-link');
+const productionsLinks = document.querySelectorAll('.productions__name-link');
+const aboutLinks = document.querySelectorAll('.about__name-link');
+const blurLink = document.querySelector('.blur-link');
 
-    function removeActiveClasses(element, list, blurLink) {
-        element.classList.remove('active');
-        list.classList.remove('active');
-        if (!solutionsLinkIsActive() && !productionsLinkIsActive() && !element.classList.contains('active')) {
-            blurLink.classList.remove('active');
-        }
-    }
-
-    function solutionsLinkIsActive() {
-        return nameLinkSolutions.classList.contains('active');
-    }
-
-    function productionsLinkIsActive() {
-        return nameLinkProductions.classList.contains('active');
-    }
-
-    function createMouseoverHandler(element, list, blurLink, nextElement) {
-        return function() {
-            removeActiveClasses(element, list, blurLink);
-            addActiveClasses(nextElement, list, blurLink);
-        };
-    }
-
-    function createMouseleaveHandler(element, list) {
-        return function() {
-            removeActiveClasses(element, list, blurLink);
-        };
-    }
-
-    var nameLinkSolutions = document.querySelector('.solutions__name-link');
-    var solutionsList = document.querySelector('.solutions__list');
-    var blurLink = document.querySelector('.blur-link');
-    var nameLinkProductions = document.querySelector('.productions__name-link');
-    var productionsList = document.querySelector('.productions__list');
-
-    nameLinkSolutions.addEventListener('mouseover', function() {
-        addActiveClasses(nameLinkSolutions, solutionsList, blurLink);
+if (window.innerWidth > minScreenWidth) {
+  const addMouseoverEvent = (link, list) => {
+    link.addEventListener('mouseover', () => {
+      resetActiveClasses();
+      link.classList.add('active');
+      list.classList.add('active');
+      if (blurLink) blurLink.classList.add('active');
+      document.body.classList.add('hidden');
     });
-    nameLinkSolutions.addEventListener('mouseleave', function() {
-        removeActiveClasses(nameLinkSolutions, solutionsList, blurLink);
-    });
+  };
 
-    nameLinkProductions.addEventListener('mouseover', function() {
-        addActiveClasses(nameLinkProductions, productionsList, blurLink);
-    });
-    nameLinkProductions.addEventListener('mouseleave', function() {
-        removeActiveClasses(nameLinkProductions, productionsList, blurLink);
-    });
+  solutionsLinks.forEach((link) => addMouseoverEvent(link, document.querySelector('.solutions__list')));
+  productionsLinks.forEach((link) => addMouseoverEvent(link, document.querySelector('.productions__list')));
+  aboutLinks.forEach((link) => addMouseoverEvent(link, document.querySelector('.about__list')));
 
-    blurLink.addEventListener('mouseenter', function() {
-        // Добавьте здесь логику, если необходимо
+  if (blurLink) {
+    blurLink.addEventListener('mouseover', () => {
+      resetActiveClasses();
+      document.body.classList.remove('hidden');
     });
+  }
 
-    var nextElementAfterNameLinkProductions = nameLinkProductions.nextElementSibling;
-
-    if (nextElementAfterNameLinkProductions) {
-        nextElementAfterNameLinkProductions.addEventListener('mouseover', createMouseoverHandler(nameLinkProductions, productionsList, blurLink, nextElementAfterNameLinkProductions));
-        nextElementAfterNameLinkProductions.addEventListener('mouseleave', createMouseleaveHandler(nextElementAfterNameLinkProductions, productionsList));
+  window.addEventListener('resize', () => {
+    if (window.innerWidth <= minScreenWidth) {
+      resetActiveClasses();
+      document.body.classList.remove('hidden');
     }
-});
+  });
+}
+
+function resetActiveClasses() {
+  const removeActiveClasses = (links, list) => {
+    links.forEach((link) => link.classList.remove('active'));
+    if (list) list.classList.remove('active');
+  };
+
+  removeActiveClasses(solutionsLinks, document.querySelector('.solutions__list'));
+  removeActiveClasses(productionsLinks, document.querySelector('.productions__list'));
+  removeActiveClasses(aboutLinks, document.querySelector('.about__list'));
+
+  if (blurLink) blurLink.classList.remove('active');
+}
 
     /**
  * Swiper 11.0.4
