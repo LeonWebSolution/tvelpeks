daccf0eb6dec6c95fc2a2f3dea617115ea20a385
LeonWebSolution <leonwebdevv@gmail.com>
2023-11-24 08:00:55 +0300

    new pages

--- src/js/components/zoomed-main.js
+++ src/js/components/zoomed-main.js
@@ -6,6 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
             if (zoomedImage) {
                 zoomedImage.classList.remove("zoomed");
             }
-        }, 1000);
+        }, 3000);
     }
 });
