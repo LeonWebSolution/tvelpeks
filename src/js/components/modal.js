7b4debc22a04cd7e664df26d53f253bbd768d143
LeonWebSolution <leonwebdevv@gmail.com>
2023-12-20 10:03:25 +0300

    fix

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,28 +1,37 @@
 export class Modal {
     constructor() {
       document.addEventListener('DOMContentLoaded', function () {
-        // Получаем все кнопки с классом 'open-modal'
+        
         var openModalButtons = document.querySelectorAll('.open-modal');
         var closeModalBtn = document.getElementById('closeModalBtn');
         var modalQuestion = document.getElementById('modal-question');
         var body = document.body;
   
         function openModal() {
-          modalQuestion.classList.add('active');
-          body.classList.add('overflow-hidden');
+          if (modalQuestion) {
+            modalQuestion.classList.add('active');
+          }
+          if (body) {
+            body.classList.add('overflow-hidden');
+          }
         }
   
         function closeModal() {
-          modalQuestion.classList.remove('active');
-          body.classList.remove('overflow-hidden');
+          if (modalQuestion) {
+            modalQuestion.classList.remove('active');
+          }
+          if (body) {
+            body.classList.remove('overflow-hidden');
+          }
         }
   
-        // Назначаем обработчик события для каждой кнопки
         openModalButtons.forEach(function (button) {
           button.addEventListener('click', openModal);
         });
   
-        closeModalBtn.addEventListener('click', closeModal);
+        if (closeModalBtn) {
+          closeModalBtn.addEventListener('click', closeModal);
+        }
       });
     }
   }
